81954424d36215f29b0d48651226354e850c6a6c
Edi Modrić <edi@netgen.hr>
2015-09-10 20:40:30 +0200

    Remove usage of deprecated settings

--- ezpublish_legacy/ngadminui/settings/design.ini.append.php
+++ ezpublish_legacy/ngadminui/settings/design.ini.append.php
@@ -2,13 +2,4 @@
 
 [ExtensionSettings]
 DesignExtensions[]=ngadminui
-
-[StylesheetSettings]
-#BackendCSSFileList[]=anglr.css
-#BackendCSSFileList[]=ngui.css
-#EditorDialogCSSFileList[]=nguieditor.css
-
-[JavaScriptSettings]
-#BackendJavaScriptList[]=anglr.js
-
 */ ?>
