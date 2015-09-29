331d582b02c946631004c8cec8c5b4fdd882ab1d
Goran Martinjak <goran@netgen.hr>
2015-09-29 14:29:34 +0200

    added ezflow layout icons

---
+++ ezpublish_legacy/ngadminui/settings/zone.ini.append.php
@@ -0,0 +1,22 @@
+<?php /* #?ini charset="utf-8"?
+
+[1ZonesA]
+
+ZoneThumbnail=1_zones_a.png
+
+[4ZonesA]
+ZoneThumbnail=4_zones_a.png
+
+[4ZonesB]
+ZoneThumbnail=4_zones_b.png
+
+[4ZonesC]
+ZoneThumbnail=4_zones_c.png
+
+[6ZonesA]
+ZoneThumbnail=6_zones_a.png
+
+[NGRegionLayout]
+ZoneThumbnail=ngmoreregionlayout.png
+
+*/ ?>
