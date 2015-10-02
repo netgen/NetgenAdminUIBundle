f9ece38bed0efda194c6994dc0694c5870ce074a
Edi Modrić <edi@netgen.hr>
2015-10-02 11:48:33 +0200

    Remove old and deprecated items from setup menu

---
+++ ezpublish_legacy/ngadminui/settings/menu.ini.append.php
@@ -0,0 +1,24 @@
+<?php /* #?ini charset="utf-8"?
+
+[Leftmenu_setup]
+Links[]
+Links[cache]=setup/cache
+Links[classes]=class/grouplist
+Links[collected]=infocollector/overview
+Links[extensions]=setup/extensions
+Links[ini]=settings/view
+Links[languages]=content/translations
+Links[packages]=package/list
+Links[rss]=rss/list
+Links[search_statistics]=search/stats
+Links[sections]=section/list
+Links[states]=state/groups
+Links[system_information]=setup/info
+Links[upgrade_check]=setup/systemupgrade
+Links[triggers]=trigger/list
+Links[url_management]=url/list
+Links[url_translator]=content/urltranslator
+Links[url_wildcards]=content/urlwildcards
+Links[workflows]=workflow/grouplist
+Links[workflow_processes]=workflow/processlist
+*/ ?>
