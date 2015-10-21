e992d2499485024b04c10a28399db2139bf307da
Edi Modrić <edi@netgen.hr>
2015-10-21 11:21:39 +0200

    Some icon tweaks

--- ezpublish_legacy/ngadminui/settings/icon.ini.append.php
+++ ezpublish_legacy/ngadminui/settings/icon.ini.append.php
@@ -1,20 +1,7 @@
 <?php /* #?ini charset="utf-8"?
 
 [IconSettings]
-
-# Where to look for icons, relative to eZ Publish
-
 Repository=extension/ngadminui/design/ngadminui/images/icons
-
-# Which theme to use as current for all icon types
-# If you wish to have a different theme per type you
-# can set this variable in the INI group for the type.
-# Wanted size of icons, the size is not checked and is expected
-# to be just a string which should be a directory in the theme
-# Defined sizes, each size refers to the name of the subdirectory
-# of the icon theme. If the name contains two numbers with an
-# x in between it will be considered to be the width and height
-# of the icon, if not no size is will be given
 
 Sizes[huge]=128x128
 Sizes[large]=48x48
@@ -26,15 +13,10 @@ Sizes[whiteNormal]=32x32;32x32/white
 Sizes[whiteSmall]=16x16;16x16/white
 
 [MimeIcons]
-
 Theme=kp
 Size=normal
 
-# Default icon if there is no mimetype match
 Default=mimetypes/binary.png
-# Mapping from a mimetype to it's icon file, the whole filename
-# must be included
-# Specifying just the group of the mimetype is also possible
 MimeMap[]
 MimeMap[text]=mimetypes/ascii.png
 MimeMap[image]=Picture.png
@@ -55,24 +37,20 @@ MimeMap[video/quicktime]=mimetypes/quicktime.png
 MimeMap[video/video/vnd.rn-realvideo]=mimetypes/real_doc.png
 
 [ClassIcons]
-
 Theme=kp
 Size=normal
 
-# Default icon if there is no class match
-Default=3d.png
-# Mapping from a class identifier to it's icon file,
-# the whole filename must be included
+Default=Document.png
 ClassMap[]
 ClassMap[article]=Article.png
 ClassMap[comment]=Chat.png
 ClassMap[common_ini_settings]=Settings.png
-ClassMap[file]=Hard-drive.png
+ClassMap[file]=Attachment.png
 ClassMap[folder]=Folder.png
-ClassMap[forum]=Folder.png
+ClassMap[forum]=Chat-active.png
 ClassMap[forum_message]=Chat.png
 ClassMap[forum_reply]=Chat.png
-ClassMap[forum_topic]=Chat.png
+ClassMap[forum_topic]=Chat-active.png
 ClassMap[frontpage]=Layout.png
 ClassMap[gallery]=Pictures-alt-2b.png
 ClassMap[image]=Picture.png
@@ -82,7 +60,7 @@ ClassMap[review]=Chat.png
 ClassMap[template_look]=Settings.png
 ClassMap[user]=User.png
 ClassMap[user_group]=Users.png
-ClassMap[weblog]=Document.png
+ClassMap[weblog]=Globe-alt.png
 ClassMap[windows_media]=Video.png
 ClassMap[block_edit]=Settings.png
 ClassMap[event]=Calendar.png
@@ -96,7 +74,7 @@ ClassMap[ng_banner]=Frame-alt.png
 ClassMap[ng_banner_video]=Frame-alt.png
 ClassMap[ng_blog_post]=Article.png
 ClassMap[ng_category]=Folder.png
-ClassMap[ng_category_page]=Folder.png
+ClassMap[ng_category_page]=Layout.png
 ClassMap[ng_container]=Folder.png
 ClassMap[ng_feedback_form]=Autofill.png
 ClassMap[ng_frontpage]=Home.png
@@ -114,11 +92,9 @@ ClassMap[ng_video]=Movie-alt.png
 ClassMap[ng_video_external]=Movie-alt.png
 
 [ClassGroupIcons]
-
 Theme=kp
 Size=normal
 
-# Default icon if there is no class group match
 Default=Folder.png
 ClassGroupMap[]
 ClassGroupMap[content]=Folder.png
@@ -127,11 +103,9 @@ ClassGroupMap[media]=Playlist.png
 ClassGroupMap[setup]=Tools.png
 
 [Icons]
-
 Theme=kp
 Size=normal
 
-# Default icon if there is no misc match
 Default=Document.png
 IconMap[]
 IconMap[role]=Users--alt.png
@@ -142,7 +116,6 @@ IconMap[url]=Globe.png
 
 [FlagIcons]
 Repository=extension/ngadminui/design/ngadminui/images/icons
-# Which theme to use for flags
 Theme=flags-flat
 IconFormat=gif
 */ ?>
