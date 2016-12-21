81c42d0d20478010b68ec37756ccf4d7673d48e9
Edi Modrić <edi@netgen.hr>
2016-12-21 15:45:33 +0100

    Add TagsBundle support

---
+++ EventListener/SetTagsAdminPageLayoutListener.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Netgen\Bundle\AdminUIBundle\EventListener;
+
+use Netgen\TagsBundle\Templating\Twig\AdminGlobalVariable;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class SetTagsAdminPageLayoutListener implements EventSubscriberInterface
+{
+    /**
+     * @var \Netgen\TagsBundle\Templating\Twig\AdminGlobalVariable
+     */
+    protected $globalVariable;
+
+    /**
+     * @var string
+     */
+    protected $pageLayoutTemplate;
+
+    /**
+     * @var bool
+     */
+    protected $isAdminSiteAccess;
+
+    /**
+     * Constructor.
+     *
+     * @param \Netgen\TagsBundle\Templating\Twig\AdminGlobalVariable $globalVariable
+     * @param string $pageLayoutTemplate
+     * @param bool $isAdminSiteAccess
+     */
+    public function __construct(
+        AdminGlobalVariable $globalVariable,
+        $pageLayoutTemplate,
+        $isAdminSiteAccess = false
+    ) {
+        $this->globalVariable = $globalVariable;
+        $this->pageLayoutTemplate = $pageLayoutTemplate;
+        $this->isAdminSiteAccess = $isAdminSiteAccess;
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(KernelEvents::REQUEST => 'onKernelRequest');
+    }
+
+    /**
+     * Sets the Netgen Tags admin pagelayout for Netgen Admin UI.
+     *
+     * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        if (!$this->isAdminSiteAccess) {
+            return;
+        }
+
+        $currentRoute = $event->getRequest()->attributes->get('_route');
+        if (mb_stripos($currentRoute, 'netgen_tags_admin') !== 0) {
+            return;
+        }
+
+        $this->globalVariable->setPageLayoutTemplate($this->pageLayoutTemplate);
+    }
+}
---
+++ MenuPlugin/TagsMenuPlugin.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Netgen\Bundle\AdminUIBundle\MenuPlugin;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class TagsMenuPlugin implements MenuPluginInterface
+{
+    /**
+     * @var array
+     */
+    protected $enabledBundles;
+
+    /**
+     * Constructor.
+     *
+     * @param array $enabledBundles
+     */
+    public function __construct(array $enabledBundles)
+    {
+        $this->enabledBundles = $enabledBundles;
+    }
+
+    /**
+     * Returns plugin identifier.
+     *
+     * @return string
+     */
+    public function getIdentifier()
+    {
+        return 'tags';
+    }
+
+    /**
+     * Returns the list of templates this plugin supports.
+     *
+     * @return array
+     */
+    public function getTemplates()
+    {
+        return array(
+            'aside' => 'NetgenAdminUIBundle:menu/plugins/tags:aside.html.twig',
+            'left' => 'NetgenAdminUIBundle:menu/plugins/tags:left.html.twig',
+        );
+    }
+
+    /**
+     * Returns if the menu is active.
+     *
+     * @return bool
+     */
+    public function isActive()
+    {
+        return isset($this->enabledBundles['NetgenTagsBundle']);
+    }
+
+    /**
+     * Returns if this plugin matches the current request.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return bool
+     */
+    public function matches(Request $request)
+    {
+        return mb_stripos(
+            $request->attributes->get('_route'),
+            'netgen_tags_admin'
+        ) === 0;
+    }
+}
