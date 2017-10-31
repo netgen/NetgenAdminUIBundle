ce57b69f30ad39d3fc6d298ec3ded1c4c5319bec
Randy Čupić <randy@netgen.hr>
2017-10-31 13:34:19 +0100

    Add tests for PathHelper class

---
+++ Tests/Helper/PathHelperTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Netgen\Bundle\AdminUIBundle\Tests\Helper;
+
+use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\Values\Content\ContentInfo;
+use eZ\Publish\Core\Base\Exceptions\UnauthorizedException;
+use eZ\Publish\Core\Helper\TranslationHelper;
+use eZ\Publish\Core\Repository\Values\Content\Location;
+use Netgen\Bundle\AdminUIBundle\Helper\PathHelper;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\RouterInterface;
+
+class PathHelperTest extends TestCase
+{
+    /**
+     * @var \Netgen\Bundle\AdminUIBundle\Helper\PathHelper
+     */
+    protected $helper;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $locationService;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $translationHelper;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $router;
+
+    /**
+     * @var \eZ\Publish\Core\Repository\Values\Content\Location
+     */
+    protected $rootLocation;
+
+    public function setUp()
+    {
+        $this->locationService = $this->createMock(LocationService::class);
+        $this->translationHelper = $this->createMock(TranslationHelper::class);
+        $this->router = $this->createMock(RouterInterface::class);
+
+        $this->rootLocation = new Location(array(
+            'id' => 2,
+            'path' => array(1, 2),
+            'contentInfo' => new ContentInfo(array(
+                'name' => 'Test Root Name',
+                'id' => 8,
+            )),
+        ));
+
+        $this->helper = new PathHelper($this->locationService, $this->translationHelper, $this->router);
+        $this->helper->setRootLocationId($this->rootLocation->id);
+    }
+
+    public function testGetPath()
+    {
+        $location = new Location(array(
+            'id' => 5,
+            'path' => array(1, 2, 5),
+            'contentInfo' => new ContentInfo(array(
+                'name' => 'Test Content Name',
+                'id' => 10,
+            )),
+        ));
+
+        $this->locationService
+            ->expects($this->exactly(3))
+            ->method('loadLocation')
+            ->withConsecutive(
+                array($location->id),
+                array($this->rootLocation->id),
+                array($location->id)
+            )
+            ->willReturnOnConsecutiveCalls(
+                $location,
+                $this->rootLocation,
+                $location
+            )
+        ;
+
+        $this->translationHelper
+            ->expects($this->exactly(2))
+            ->method('getTranslatedContentNameByContentInfo')
+            ->withConsecutive(array($this->rootLocation->getContentInfo()), array($location->getContentInfo()))
+            ->willReturnOnConsecutiveCalls($this->rootLocation->getContentInfo()->name, $location->getContentInfo()->name)
+        ;
+
+        $this->router
+            ->expects($this->once())
+            ->method('generate')
+            ->with($this->rootLocation)
+            ->willReturn('/root_location_url')
+        ;
+
+        $expectedResponse = array(
+            array(
+                'text' => $this->rootLocation->getContentInfo()->name,
+                'url' => '/root_location_url',
+                'locationId' => $this->rootLocation->id,
+                'contentId' => $this->rootLocation->getContentInfo()->id,
+            ),
+            array(
+                'text' => $location->getContentInfo()->name,
+                'url' => false,
+                'locationId' => $location->id,
+                'contentId' => $location->getContentInfo()->id,
+            ),
+        );
+
+        $this->assertEquals($expectedResponse, $this->helper->getPath($location->id));
+    }
+
+    public function testGetPathUnauthorized()
+    {
+        $location = new Location(array(
+            'id' => 5,
+            'path' => array(1, 7, 2, 5),
+        ));
+
+        $this->locationService
+            ->expects($this->exactly(2))
+            ->method('loadLocation')
+            ->withConsecutive(
+                array($location->id),
+                array($this->rootLocation->id)
+            )
+            ->willReturnCallback(
+                function ($parameter) use ($location) {
+                    if ($parameter === $location->id) {
+                        return $location;
+                    }
+                    throw new UnauthorizedException('test_module', 'test_function');
+                }
+            )
+        ;
+
+        $this->assertEquals(array(), $this->helper->getPath($location->id));
+    }
+}
