9919ed4abad03281a88475d4e1c13f2153af748f
Ljudevit <ljudevit@cjdizajn.hr>
2020-01-08 14:05:11 +0100

    Add Layouts tab for objects set in media section (#35)

--- bundle/ezpublish_legacy/ngadminui/settings/admininterface.ini.append.php
+++ bundle/ezpublish_legacy/ngadminui/settings/admininterface.ini.append.php
@@ -2,11 +2,19 @@
 
 [WindowControlsSettings]
 AdditionalTabs[]=nglayouts
+AdditionalTabs[]=nglayouts_media
 
 [AdditionalTab_nglayouts]
 Title=
 Description=
 NavigationPartName=ezcontentnavigationpart
+HeaderTemplate=nglayouts_header.tpl
+Template=nglayouts.tpl
+
+[AdditionalTab_nglayouts_media]
+Title=
+Description=
+NavigationPartName=ezmedianavigationpart
 HeaderTemplate=nglayouts_header.tpl
 Template=nglayouts.tpl
 */
